cd325696202f035eb3a55b608b255f1351d26572
jpgithub2023 <134462870+jpgithub2023@users.noreply.github.com>
2023-10-05 23:39:27 -0400

    First commit

---
+++ src/strings/Str.java
@@ -0,0 +1,23 @@
+package strings;
+
+import java.util.Scanner;
+
+public class Str {
+
+	public static void main(String[] args) {
+		
+		Scanner ui=new Scanner(System.in);
+		
+		System.out.println("Please enter a String:");
+		String a=ui.nextLine();
+		String b= a.toUpperCase();
+		System.out.println(b);
+		System.out.println(b.length());
+		String c=b.replace('O', 'Z');
+		System.out.println(c);
+		System.out.println(c.charAt(c.length()-1));
+		System.out.println(a.concat(" test"));
+
+	}
+
+}
